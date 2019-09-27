96ba67f8ed5a721c56d2a4f93512654d64f4ce66
Jincheng-Sun <sjc951213@gmail.com>
2019-09-27 16:23:10 -0400

    update init function

--- framework/model.py
+++ framework/model.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 import torch
 class Model():
     def __init__(self, save_path):
-        self.save_opath = save_path
+        self.save_path = save_path
         self.model = None
     def check_cuda(self):
         if torch.cuda.is_available():
@@ -20,17 +20,17 @@ class Model():
     def evaluate(self, **kwargs):
         pass
 
-    def save_checkpoint(self,ckpt_dict, checkpoint_path, epoch):
+    def save_checkpoint(self,ckpt_dict, checkpoint_path):
         torch.save(ckpt_dict, checkpoint_path)
 
     def resume_checkpoint(self, checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
         self.model.load_state_dict(checkpoint['model_state_dict'])
-        self.model.train()
+        # self.model.train()
 
-    def save_model(self, save_path):
-        torch.save(self.model.state_dict(), save_path)
+    def save_model(self, checkpoint, save_path):
+        torch.save(checkpoint, save_path)
 
     def load_model(self, model_path):
         self.model.load_state_dict(torch.load(model_path))
-        self.model.eval()
+        # self.model.eval()
