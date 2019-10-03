15bf9a9ca41e219bc55b2b944ad4185b25896c29
Jincheng-Sun <sjc951213@gmail.com>
2019-10-03 16:39:47 -0400

    finish Transformer model

--- Layers/transformer.py
+++ Layers/transformer.py
@@ -2,22 +2,37 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+
 class EncoderLayer(nn.Module):
     def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
         super().__init__()
-        self.slf_attn = MultiHeadAttention(
+        self.self_attn = MultiHeadAttention(
             n_head, d_model, d_k, d_v, dropout=dropout)
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
 
     def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None):
-        enc_output, enc_slf_attn = self.slf_attn(
+        '''
+            Arguments:
+                enc_input {Tensor, shape [batch, length, embedding_length]} -- input
+                non_pad_mask {Tensor, shape [batch, length, 1]} -- index of which position in a sequence is a padding
+                slf_attn_mask {Tensor, shape [batch, q_length, k_length]} -- self attention mask
+            Returns:
+                enc_output {Tensor, shape [batch, q_length, embedding_length]} -- output
+                encoder_self_attn {n_head * batch, q_length, k_length}
+
+        '''
+        enc_output, encoder_self_attn = self.self_attn(
             enc_input, enc_input, enc_input, mask=slf_attn_mask)
         enc_output *= non_pad_mask
 
-        enc_output = self.pos_ffn(enc_output)
+        enc_output = self.pos_ffn(enc_output) # wider the network
         enc_output *= non_pad_mask
 
-        return enc_output, enc_slf_attn
+        # enc_output:       [batch_size, seq_length, w2v_length]
+        # non_pad_mask:     [n_head, seq_length, 1]
+        # slf_attn_mask:    [batch_size, seq_length, seq_length]
+        return enc_output, encoder_self_attn
+
 
 class DecoderLayer(nn.Module):
     ''' Compose with three layers '''
@@ -29,6 +44,9 @@ class DecoderLayer(nn.Module):
         self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
 
     def forward(self, dec_input, enc_output, non_pad_mask=None, slf_attn_mask=None, dec_enc_attn_mask=None):
+        # dec_input:        [batch_size, tgt_length, w2v_length]
+        # non_pad_mask:     [batch_size, tgt_length, 1]
+        # slf_attn_mask:    [batch_size, tgt_length, seq_length]
         dec_output, dec_slf_attn = self.slf_attn(
             dec_input, dec_input, dec_input, mask=slf_attn_mask)
         dec_output *= non_pad_mask
@@ -37,10 +55,14 @@ class DecoderLayer(nn.Module):
             dec_output, enc_output, enc_output, mask=dec_enc_attn_mask)
         dec_output *= non_pad_mask
 
-        dec_output = self.pos_ffn(dec_output)
+        dec_output = self.pos_ffn(dec_output) # wider the network
         dec_output *= non_pad_mask
 
+        # dec_output:       [batch_size, seq_length, w2v_length]
+        # non_pad_mask:     [n_head, seq_length, 1]
+        # slf_attn_mask:    [batch_size, seq_length, seq_length]
         return dec_output, dec_slf_attn, dec_enc_attn
+
 
 class ScaledDotProductAttention(nn.Module):
     ''' Scaled Dot-Product Attention '''
@@ -52,18 +74,30 @@ class ScaledDotProductAttention(nn.Module):
         self.softmax = nn.Softmax(dim=2)
 
     def forward(self, query, key, value, mask=None):
+        '''
+            Arguments:
+                query {Tensor, shape [n_head * batch, q_length, dk]} -- query
+                key {Tensor, shape [n_head * batch, k_length, dk]} -- key
+                value {Tensor, shape [n_head * batch, v_length, dv]} -- value
+                mask {Tensor, shape [n_head * batch, q_length, k_length]} --self attn mask
 
-        attn = torch.bmm(query, key.transpose(1, 2))
+            Returns:
+                output {Tensor, shape [n_head * batch, q_length, dv] -- output
+                attn {Tensor, shape [n_head * batch, q_length, k_length] -- self attention
+
+        '''
+        attn = torch.bmm(query, key.transpose(1, 2)) # [n_head * batch, q_length, k_length]
         attn = attn / self.temperature
 
         if mask is not None:
             attn = attn.masked_fill(mask, -np.inf)
 
-        attn = self.softmax(attn)
+        attn = self.softmax(attn) # softmax over k_length
         attn = self.dropout(attn)
         output = torch.bmm(attn, value)
 
         return output, attn
+
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
@@ -88,51 +122,69 @@ class MultiHeadAttention(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, query, key, value, mask=None):
+        '''
+            Arguments:
+                query {Tensor, shape [batch, q_length, embedding_length]} -- query
+                key {Tensor, shape [batch, k_length, embedding_length]} -- key
+                value {Tensor, shape [batch, v_length, embedding_length]} -- value
+                mask {Tensor, shape [batch, q_length, k_length]} --self attn mask
 
+            Returns:
+                output {Tensor, shape [batch, q_length, embedding_length]} -- output
+                attn {Tensor, shape [n_head * batch, q_length, k_length] -- self attention
+        '''
         d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
 
         sz_b, len_q, _ = query.size()
         sz_b, len_k, _ = key.size()
         sz_b, len_v, _ = value.size()
 
+        assert len_k == len_v
+
         residual = query
 
-        query = self.w_qs(query).view(sz_b, len_q, n_head, d_k)
+        query = self.w_qs(query).view(sz_b, len_q, n_head, d_k)  # target
+        # [batch_size, seq_length, w2v_length] -> [batch_size, seq_length, n_head * dk] -> [batch_size, seq_length, n_head, dk]
         key = self.w_ks(key).view(sz_b, len_k, n_head, d_k)
         value = self.w_vs(value).view(sz_b, len_v, n_head, d_v)
 
-        query = query.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k) # (n*b) x lq x dk
-        key = key.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k) # (n*b) x lk x dk
-        value = value.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v) # (n*b) x lv x dv
+        query = query.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k)  # [n_head * batch_size, seq_length, dk]
+        key = key.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k)  # (n*b) x lk x dk
+        value = value.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v)  # (n*b) x lv x dv
 
-        mask = mask.repeat(n_head, 1, 1) # (n*b) x .. x ..
+        mask = mask.repeat(n_head, 1, 1)  # [n_head * batch_size, seq_length, seq_length]
         output, attn = self.attention(query, key, value, mask=mask)
 
         output = output.view(n_head, sz_b, len_q, d_v)
-        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1) # b x lq x (n*dv)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1)  # b x lq x (n*dv)
 
         output = self.dropout(self.fc(output))
         output = self.layer_norm(output + residual)
 
         return output, attn
 
+
 class PositionwiseFeedForward(nn.Module):
     ''' A two-feed-forward-layer module '''
 
     def __init__(self, d_in, d_hid, dropout=0.1):
         super().__init__()
-        self.w_1 = nn.Conv1d(d_in, d_hid, 1) # position-wise
-        self.w_2 = nn.Conv1d(d_hid, d_in, 1) # position-wise
+        self.w_1 = nn.Conv1d(d_in, d_hid, 1)  # position-wise
+        self.w_2 = nn.Conv1d(d_hid, d_in, 1)  # position-wise
         self.layer_norm = nn.LayerNorm(d_in)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
+        '''
+            Arguments:
+            x {Tensor, shape [batch_size, q_length, embedding_length]}
+
+        '''
         residual = x
         output = x.transpose(1, 2)
-        output = self.w_2(nn.functional.relu(self.w_1(output)))
+        output = nn.functional.relu(self.w_1(output))
+        output = self.w_2(output)
         output = output.transpose(1, 2)
         output = self.dropout(output)
         output = self.layer_norm(output + residual)
         return output
-
-
--- Models/transformer.py
+++ Models/transformer.py
@@ -1,143 +1,149 @@
 from framework.model import Model
-from Modules.transformer import Transformer
+from Modules.transformer import Transformer, TransformerCls
 from Optimizer.adam import AdamOptimizer
-import numpy as np
+import torch.nn as nn
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 import math
 import time
 from utils.loggings import logger
+from abc import abstractmethod
 
 
 class TransformerModel(Model):
-    def __init__(self, save_path, log_path,
-                 n_src_vocab, n_tgt_vocab, len_max_seq,
-                 d_word_vec=512, d_model=512, d_inner=2048,
-                 n_layers=6, n_head=8, d_k=64, d_v=64, dropout=0.1,
-                 warmup_step=100, lr=0.01,
-                 tgt_emb_prj_weight_sharing=True,
-                 emb_src_tgt_weight_sharing=True):
+    def __init__(self, save_path, log_path):
         super().__init__(save_path=save_path)
 
-        self.model = Transformer(n_src_vocab, n_tgt_vocab, len_max_seq,
-                                 d_word_vec, d_model, d_inner,
-                                 n_layers, n_head, d_k, d_v, dropout,
-                                 tgt_emb_prj_weight_sharing,
-                                 emb_src_tgt_weight_sharing)
+        self.model = None
+        self.optimizer = None
+        self._logger_train = logger(log_path + 'train')
+        self._logger_eval = logger(log_path + 'test')
 
-        self.optimizer = AdamOptimizer(self.model.parameters(), warmup_step, lr_max=lr)
-
-        self.logger_train = logger(log_path + 'train')
-        self.logger_eval = logger(log_path + 'test')
-
-    def loss(self, pred, real, smoothing):
+    def loss_cross_entropy(self, logits, real, smoothing=False):
         ''' Calculate cross entropy loss, apply label smoothing if needed. '''
 
         real = real.contiguous().view(-1)
 
         if smoothing:
             eps = 0.1
-            n_class = pred.size(1)
+            n_class = logits.size(1)
 
-            one_hot = torch.zeros_like(pred).scatter(1, real.view(-1, 1), 1)
+            one_hot = torch.zeros_like(logits).scatter(1, real.view(-1, 1), 1)
             one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
-            log_prb = F.log_softmax(pred, dim=1)
+            log_prb = F.log_softmax(logits, dim=1)
 
             non_pad_mask = real.ne(0)
             loss = -(one_hot * log_prb).sum(dim=1)
             loss = loss.masked_select(non_pad_mask).sum()  # average later
         else:
-            loss = F.cross_entropy(pred, real, ignore_index=0, reduction='sum')
+            loss = F.cross_entropy(logits, real, ignore_index=0, reduction='sum')
 
         return loss
 
-    def cal_performance(self, pred, real, smoothing=False):
+    def performance_multi(self, logits, real):
         ''' Apply label smoothing if needed '''
 
-        loss = self.loss(pred, real, smoothing)
-
-        pred = pred.max(1)[1]
+        logits = logits.max(1)[1]
         real = real.contiguous().view(-1)
         non_pad_mask = real.ne(0)
-        n_correct = pred.eq(real)
+        n_correct = logits.eq(real)
         n_correct = n_correct.masked_select(non_pad_mask).sum().item()
 
-        return loss, n_correct
+        return n_correct
 
+    def loss_regression(self, logits, real):
+        loss = F.mse_loss(logits, real)
+
+        return loss
+
+    def performance_regression(self, logits, real, threshold):
+        ''' Apply label smoothing if needed '''
+
+        logits = logits.ge(threshold)
+        real = real.contiguous().view(-1)
+        non_pad_mask = real.ne(0)
+        n_correct = logits.eq(real)
+        n_correct = n_correct.masked_select(non_pad_mask).sum().item()
+
+        return n_correct
+
+
+    @abstractmethod
     def train_epoch(self, training_data, device, smoothing):
         ''' Epoch operation in training phase'''
+        pass
+        # self.model.train()
+        #
+        # total_loss = 0
+        # n_word_total = 0
+        # n_word_correct = 0
+        #
+        # for batch in tqdm(
+        #         training_data, mininterval=2,
+        #         desc='  - (Training)   ', leave=False):  # process bar
+        #     # prepare data
+        #     src_seq, src_pos, tgt_seq, tgt_pos = map(lambda x: x.to(device), batch)
+        #     real = tgt_seq[:, 1:]
+        #
+        #     # forward
+        #     self.optimizer.zero_grad()
+        #     pred = self.model(src_seq, src_pos, tgt_seq, tgt_pos)
+        #
+        #     # backward
+        #     loss, n_correct = self.cal_performance_multi(pred, real, smoothing=smoothing)
+        #     loss.backward()
+        #
+        #     # update parameters
+        #     self.optimizer.step()
+        #
+        #     # note keeping
+        #     total_loss += loss.item()
+        #
+        #     non_pad_mask = real.ne(0)
+        #     n_word = non_pad_mask.sum().item()
+        #     n_word_total += n_word
+        #     n_word_correct += n_correct
+        #
+        # loss_per_word = total_loss / n_word_total
+        # accuracy = n_word_correct / n_word_total
+        # return loss_per_word, accuracy
 
-        self.model.train()
-
-        total_loss = 0
-        n_word_total = 0
-        n_word_correct = 0
-
-        for batch in tqdm(
-                training_data, mininterval=2,
-                desc='  - (Training)   ', leave=False):  # process bar
-            # prepare data
-            src_seq, src_pos, tgt_seq, tgt_pos = map(lambda x: x.to(device), batch)
-            real = tgt_seq[:, 1:]
-
-            # forward
-            self.optimizer.zero_grad()
-            pred = self.model(src_seq, src_pos, tgt_seq, tgt_pos)
-
-            # backward
-            loss, n_correct = self.cal_performance(pred, real, smoothing=smoothing)
-            loss.backward()
-
-            # update parameters
-            self.optimizer.step()
-
-            # note keeping
-            total_loss += loss.item()
-
-            non_pad_mask = real.ne(0)
-            n_word = non_pad_mask.sum().item()
-            n_word_total += n_word
-            n_word_correct += n_correct
-
-        loss_per_word = total_loss / n_word_total
-        accuracy = n_word_correct / n_word_total
-        return loss_per_word, accuracy
-
+    @abstractmethod
     def eval_epoch(self, validation_data, device):
         ''' Epoch operation in evaluation phase '''
+        pass
+        # self.model.eval()
+        #
+        # total_loss = 0
+        # n_word_total = 0
+        # n_word_correct = 0
+        #
+        # with torch.no_grad():
+        #     for batch in tqdm(
+        #             validation_data, mininterval=2,
+        #             desc='  - (Validation) ', leave=False):
+        #         # prepare data
+        #         src_seq, src_pos, tgt_seq, tgt_pos = map(lambda x: x.to(device), batch)
+        #         gold = tgt_seq[:, 1:]
+        #
+        #         # forward
+        #         pred = self.model(src_seq, src_pos, tgt_seq, tgt_pos)
+        #         loss, n_correct = self.cal_performance_multi(pred, gold, smoothing=False)
+        #
+        #         # note keeping
+        #         total_loss += loss.item()
+        #
+        #         non_pad_mask = gold.ne(0)
+        #         n_word = non_pad_mask.sum().item()
+        #         n_word_total += n_word
+        #         n_word_correct += n_correct
+        #
+        # loss_per_word = total_loss / n_word_total
+        # accuracy = n_word_correct / n_word_total
+        # return loss_per_word, accuracy
 
-        self.model.eval()
-
-        total_loss = 0
-        n_word_total = 0
-        n_word_correct = 0
-
-        with torch.no_grad():
-            for batch in tqdm(
-                    validation_data, mininterval=2,
-                    desc='  - (Validation) ', leave=False):
-                # prepare data
-                src_seq, src_pos, tgt_seq, tgt_pos = map(lambda x: x.to(device), batch)
-                gold = tgt_seq[:, 1:]
-
-                # forward
-                pred = self.model(src_seq, src_pos, tgt_seq, tgt_pos)
-                loss, n_correct = self.cal_performance(pred, gold, smoothing=False)
-
-                # note keeping
-                total_loss += loss.item()
-
-                non_pad_mask = gold.ne(0)
-                n_word = non_pad_mask.sum().item()
-                n_word_total += n_word
-                n_word_correct += n_correct
-
-        loss_per_word = total_loss / n_word_total
-        accuracy = n_word_correct / n_word_total
-        return loss_per_word, accuracy
-
-    def train(self, training_data, validation_data, epoch, optimizer, device, smoothing, save_mode):
+    def train(self, training_data, validation_data, epoch, device, smoothing, save_mode):
         assert save_mode in ['all', 'best']
         valid_losses = []
         for epoch_i in range(epoch):
@@ -145,22 +151,22 @@ class TransformerModel(Model):
 
             start = time.time()
             train_loss, train_accu = self.train_epoch(training_data, device, smoothing=smoothing)
-            self.logger_train.info('  - (Training)   ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
-                  'elapse: {elapse:3.3f} min'.format(
+            self._logger_train.info('  - (Training)   ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
+                                    'elapse: {elapse:3.3f} min'.format(
                 ppl=math.exp(min(train_loss, 100)), accu=100 * train_accu,
                 elapse=(time.time() - start) / 60))
 
             start = time.time()
             valid_loss, valid_accu = self.eval_epoch(validation_data, device)
-            self.logger_eval.info('  - (Validation) ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
-                  'elapse: {elapse:3.3f} min'.format(
+            self._logger_eval.info('  - (Validation) ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
+                                   'elapse: {elapse:3.3f} min'.format(
                 ppl=math.exp(min(valid_loss, 100)), accu=100 * valid_accu,
                 elapse=(time.time() - start) / 60))
 
             valid_losses += [valid_loss]
 
             checkpoint = {
-                'model': self.model.state_dict(),
+                'models': self.model.state_dict(),
                 'optimizer_state_dict': self.optimizer.state_dict(),
                 'epoch': epoch_i}
 
@@ -172,3 +178,111 @@ class TransformerModel(Model):
                     self.save_model(checkpoint, self.save_path + '_loss_{loss:3.3f}.chkpt'.format(loss=valid_loss))
                     print('    - [Info] The checkpoint file has been updated.')
 
+
+class TransformerClsModel(TransformerModel):
+    def __init__(self, save_path, log_path, embedding, len_max_seq, n_tgt_vocab,
+                 d_word_vec=512, d_model=512, d_inner=2048,
+                 n_layers=6, n_head=8, d_k=64, d_v=64, dropout=0.1,
+                 warmup_step=100, lr=0.01, is_regression=False):
+
+        super().__init__(save_path, log_path)
+        self.model = TransformerCls(embedding=embedding, len_max_seq=len_max_seq,
+                                    d_word_vec=d_word_vec, d_model=d_model, d_inner=d_inner,
+                                    n_layers=n_layers, n_head=n_head, d_k=d_k, d_v=d_v,
+                                    output_num=n_tgt_vocab, dropout=dropout)
+
+        self.optimizer = AdamOptimizer(self.model.parameters(), warmup_step, lr_max=lr)
+        self.is_regression = is_regression
+
+    def train_epoch(self, training_data, device, smoothing):
+        ''' Epoch operation in training phase'''
+
+        self.model.train()
+
+        total_loss = 0
+        n_sample_total = 0
+        n_sample_correct = 0
+
+        for batch in tqdm(
+                training_data, mininterval=2,
+                desc='  - (Training)   ', leave=False):  # training_data should be a iterable
+
+            # TODO: batch iterable
+            # # prepare data
+            # src_seq, src_pos, label= map(lambda x: x.to(device), batch)
+
+            src_seq, src_pos, label = training_data
+            # forward
+            self.optimizer.zero_grad()
+            self.logits = self.model(src_seq, src_pos)
+
+            # activation
+            self.pred = nn.Sigmoid(self.logits)
+
+            # backward
+            if self.is_regression:
+                loss = self.loss_regression(self.logits, label)
+                n_correct = self.performance_regression(self.logits, label)
+
+            else:
+                loss = self.loss_cross_entropy(self.logits, label, smoothing)
+                n_correct = self.performance_multi(self.logits, label)
+
+            loss.backward()
+
+            # update parameters
+            self.optimizer.step()
+
+            # note keeping
+            total_loss += loss.item()
+
+            non_pad_mask = label.ne(0)
+            n_sample = non_pad_mask.sum().item()
+            n_sample_total += n_sample
+            n_sample_correct += n_correct
+
+        loss_per_word = total_loss / n_sample_total
+        accuracy = n_sample_correct / n_sample_total
+        return loss_per_word, accuracy
+
+    def eval_epoch(self, validation_data, device):
+        ''' Epoch operation in evaluation phase '''
+
+        self.model.eval()
+
+        total_loss = 0
+        n_sample_total = 0
+        n_sample_correct = 0
+
+        with torch.no_grad():
+            for batch in tqdm(
+                    validation_data, mininterval=2,
+                    desc='  - (Validation) ', leave=False):
+                # prepare data
+                src_seq, src_pos, label = map(lambda x: x.to(device), batch)
+
+                # forward
+                pred = self.model(src_seq, src_pos)
+
+                if self.is_regression:
+                    loss = self.loss_regression(self.logits, label)
+                    n_correct = self.performance_regression(self.logits, label)
+
+                else:
+                    loss = self.loss_cross_entropy(self.logits, label)
+                    n_correct = self.performance_multi(self.logits, label)
+
+                # note keeping
+                total_loss += loss.item()
+
+                # note keeping
+                total_loss += loss.item()
+
+                non_pad_mask = label.ne(0)
+                n_sample = non_pad_mask.sum().item()
+                n_sample_total += n_sample
+                n_sample_correct += n_correct
+
+            loss_per_word = total_loss / n_sample_total
+            accuracy = n_sample_correct / n_sample_total
+            return loss_per_word, accuracy
