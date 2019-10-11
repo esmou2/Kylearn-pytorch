52e64f5a294a2526436a82c1236f6686918a35a2
Jincheng-Sun <sjc951213@gmail.com>
2019-10-10 20:48:36 -0400

    add training control and early stopping

--- Models/reactionattention.py
+++ Models/reactionattention.py
@@ -1,238 +1,242 @@
-from Training.optimizers import AdamOptimizer
 from torch.optim.adam import Adam
 from Training.losses import *
-from Training.evaluation import accuracy, precision_racall
+from Training.evaluation import accuracy, precision_recall
+from Training.control import TrainingControl, EarlyStopping
 import torch.nn as nn
+from framework.model import Model
+from tqdm import tqdm
 
 import pytorch_lightning as pl
 
 
-class ReactionModel(pl.LightningModule):
-    def __init__(self, dataloader, stack, d_reactant, d_bottleneck, d_classifier, d_output, threshold=None,
-                 n_layers=6, n_head=8, dropout=0.1):
-        super().__init__()
+# class ReactionModelLightning(pl.LightningModule):
+#     def __init__(self, dataloader, stack, d_reactant, d_bottleneck, d_classifier, d_output, threshold=None,
+#                  n_layers=6, n_head=8, dropout=0.1):
+#         super().__init__()
+#
+#         self.dataloader = dataloader
+#         feature1_dim, feature2_dim = self.dataloader.get_feature_dim()
+#         self.model = stack(n_layers, n_head, feature1_dim, feature2_dim, d_reactant, d_bottleneck,
+#                            dropout)
+#         self.fc1 = nn.Linear(feature1_dim, d_classifier)
+#         self.fc2 = nn.Linear(d_classifier, d_output)
+#
+#         self.d_output = d_output
+#         self.threshold = threshold
+#
+#     def forward(self, feature_1, feature_2):
+#         output = self.model(feature_1, feature_2)
+#         output = self.fc1(output[0])
+#         output = nn.functional.relu(output)
+#         output = self.fc2(output)
+#         return output
+#
+#     def training_step(self, batch, batch_nb):
+#         feature_1, feature_2, y = batch
+#         logits = self.forward(feature_1, feature_2)
+#         pred = logits.sigmoid()
+#
+#         if y.shape[-1] == 1:
+#             loss = mse_loss(pred, y)
+#
+#         else:
+#             loss = cross_entropy_loss(pred, y, smoothing=True)
+#
+#         acc = accuracy(pred, y, threshold=self.threshold)
+#         _, _, precision_avg, recall_avg = precision_recall(pred, y, self.d_output, threshold=self.threshold)
+#         tensorboard_logs = {'train_loss': loss, 'train_acc': acc,
+#                             'train_precision': precision_avg,
+#                             'train_recall': recall_avg}
+#
+#         return {'loss': loss, 'acc': acc, 'precision': precision_avg, 'recall': recall_avg, 'log': tensorboard_logs}
+#
+#     def validation_step(self, batch, batch_nb):
+#         feature_1, feature_2, y = batch
+#         logits = self.forward(feature_1, feature_2)
+#         pred = logits.sigmoid()
+#
+#         if y.shape[-1] == 1:
+#             loss = mse_loss(pred, y)
+#
+#         else:
+#             loss = cross_entropy_loss(pred, y, smoothing=False)
+#
+#         acc = accuracy(pred, y, threshold=self.threshold)
+#         _, _, precision_avg, recall_avg = precision_recall(pred, y, self.d_output, threshold=self.threshold)
+#
+#         return {'val_loss': loss, 'val_acc': acc,
+#                 'val_precision': precision_avg,
+#                 'val_recall': recall_avg}
+#
+#     def validation_end(self, outputs):
+#         avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+#         avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+#         avg_precision = torch.stack([x['val_precision'] for x in outputs]).mean()
+#         avg_recall = torch.stack([x['val_recall'] for x in outputs]).mean()
+#
+#         tensorboard_logs = {'val_loss': avg_loss, 'val_acc': avg_acc,
+#                             'val_precision': avg_precision,
+#                             'val_recall': avg_recall}
+#
+#         return {'loss': avg_loss, 'acc': avg_acc, 'precision': avg_precision, 'recall': avg_recall,
+#                 'log': tensorboard_logs}
+#
+#     def configure_optimizers(self):
+#         # return AdamOptimizer(self.parameters(), 1000, 0.01)
+#         return Adam(self.parameters(), lr=0.005, betas=(0.9, 0.999))
+#
+#     @pl.data_loader
+#     def train_dataloader(self):
+#         return self.dataloader.train_dataloader()
+#
+#     @pl.data_loader
+#     def val_dataloader(self):
+#         return self.dataloader.val_dataloader()
+#
+#     @pl.data_loader
+#     def test_dataloader(self):
+#         return self.dataloader.test_dataloader()
+
+
+class ReactionModel_(Model):
+    def __init__(self, save_path, log_path, dataloader, optimizer, stack, d_reactant, d_bottleneck, d_classifier,
+                 d_output, threshold=None, n_layers=6, n_head=8, dropout=0.1):
+
+        super().__init__(save_path, log_path)
 
         self.dataloader = dataloader
         feature1_dim, feature2_dim = self.dataloader.get_feature_dim()
         self.model = stack(n_layers, n_head, feature1_dim, feature2_dim, d_reactant, d_bottleneck,
-                                            dropout)
+                           dropout)
         self.fc1 = nn.Linear(feature1_dim, d_classifier)
         self.fc2 = nn.Linear(d_classifier, d_output)
 
+        self.d_output = d_output
         self.threshold = threshold
 
-    def forward(self, feature_1, feature_2):
-        output = self.model(feature_1, feature_2)
-        output = self.fc1(output[0])
-        output = nn.functional.relu(output)
-        output = self.fc2(output)
-        return output
+        self.optimizer = optimizer
 
-    def training_step(self, batch, batch_nb):
-        feature_1, feature_2, y = batch
-        logits = self.forward(feature_1, feature_2)
-        pred = logits.sigmoid()
+        self.controller = TrainingControl(max_step=100000, evaluate_every_nstep=100, print_every_nstep=10)
+        self.early_stopping = EarlyStopping(patience=50)
 
+    def train_epoch(self, device, smothing):
+        ''' Epoch operation in training phase'''
 
-        if y.shape[-1] == 1:
-            loss = mse_loss(pred, y)
+        self.model.train()
 
-        else:
-            loss = cross_entropy_loss(pred, y, smoothing=True)
+        total_loss = 0
 
-        acc = accuracy(pred, y, threshold=self.threshold)
-        _, _, precision_avg, recall_avg = precision_racall(pred, y, threshold=self.threshold)
-        tensorboard_logs = {'train_loss': loss, 'train_acc': acc,
-                            'train_precision': precision_avg,
-                            'train_recall': recall_avg}
+        for batch in tqdm(
+                self.dataloader.train_dataloader(), mininterval=1,
+                desc='  - (Training)   ', leave=False):  # training_data should be a iterable
 
-        return {'loss': loss, 'log': tensorboard_logs}
+            # prepare data
+            batch_idx = batch[0]
+            feature_1, feature_2, y = map(lambda x: x.to(device), batch[1])
 
-    def validation_step(self, batch, batch_nb):
-        feature_1, feature_2, y = batch
-        logits = self.forward(feature_1, feature_2)
-        pred = logits.sigmoid()
+            # forward
+            self.optimizer.zero_grad()
+            logits = self.model(feature_1, feature_2)
 
-        if y.shape[-1] == 1:
-            loss = mse_loss(pred, y)
+            pred = logits.sigmoid()
 
-        else:
-            loss = cross_entropy_loss(pred, y, smoothing=False)
+            if y.shape[-1] == 1:
+                loss = mse_loss(pred, y)
 
-        acc = accuracy(pred, y, threshold=self.threshold)
-        _, _, precision_avg, recall_avg = precision_racall(pred, y, threshold=self.threshold)
+            else:
+                loss = cross_entropy_loss(pred, y, smoothing=smothing)
 
-        return {'val_loss': loss, 'val_acc': acc,
-                'val_precision': precision_avg,
-                'val_recall': recall_avg}
+            acc = accuracy(pred, y, threshold=self.threshold)
+            _, _, precision_avg, recall_avg = precision_recall(pred, y, self.d_output, threshold=self.threshold)
 
-    def validation_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
-        avg_precision = torch.stack([x['val_precision'] for x in outputs]).mean()
-        avg_recall = torch.stack([x['val_recall'] for x in outputs]).mean()
+            loss.backward()
 
-        tensorboard_logs = {'val_loss': avg_loss, 'val_acc': avg_acc,
-                            'val_precision': avg_precision,
-                            'val_recall': avg_recall}
+            # update parameters
+            self.optimizer.step()
 
-        return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
+            # note keeping
+            total_loss += loss.item()
 
-    def configure_optimizers(self):
-        # return AdamOptimizer(self.parameters(), 1000, 0.01)
-        return Adam(self.parameters(), lr=0.005, betas=(0.9, 0.999))
+            state_dict = self.controller(batch_idx)
 
-    @pl.data_loader
-    def train_dataloader(self):
-        return self.dataloader.train_dataloader()
+            if state_dict['step_to_print']:
+                self.train_logger.info(
+                    '[TRAINING] - loss: { %3.4f },    acc: { %1.4f },    pre: { %1.4f },    rec: { %1.4f }' % (
+                        loss, acc, precision_avg, recall_avg))
 
-    @pl.data_loader
-    def val_dataloader(self):
-        return self.dataloader.val_dataloader()
+            if state_dict['step_to_evaluate']:
+                stop = self.val_epoch(device, state_dict['step'])
+                state_dict['step_to_stop'] = stop
+                if stop:
+                    break
 
-    @pl.data_loader
-    def test_dataloader(self):
-        return self.dataloader.test_dataloader()
+            if self.controller.current_step == self.controller.max_step:
+                state_dict['step_to_stop'] = True
+                break
 
+        return state_dict
 
-# class ReactionModel_(Model):
-#     def __init__(self, save_path, log_path, feature1_dim, feature2_dim, d_reactant, d_bottleneck,
-#                  n_layers=6, n_head=8, dropout=0.1,
-#                  warmup_step=100, lr=0.01, is_regression=False, threshold=0):
-#
-#         super().__init__(save_path)
-#
-#         self.model = ReactionAttentionStack(n_layers, n_head, feature1_dim, feature2_dim, d_reactant, d_bottleneck,
-#                                             dropout)
-#
-#         self.optimizer = AdamOptimizer(self.model.parameters(), warmup_step, lr_max=lr)
-#         self.is_regression = is_regression
-#         self.threshold = threshold
-#
-#         self._logger_train = logger(log_path + 'train')
-#         self._logger_eval = logger(log_path + 'test')
-#
-#     def train_epoch(self, training_data, device, smoothing):
-#         ''' Epoch operation in training phase'''
-#
-#         self.model.train()
-#
-#         total_loss = 0
-#         n_sample_total = 0
-#         n_sample_correct = 0
-#
-#         for batch in tqdm(
-#                 training_data, mininterval=1,
-#                 desc='  - (Training)   ', leave=False):  # training_data should be a iterable
-#
-#             # TODO: batch iterable
-#             # prepare data
-#             index, position, target = map(lambda x: x.to(device), batch)
-#
-#             # forward
-#             self.optimizer.zero_grad()
-#             self.logits = self.model(index, position)
-#
-#             # backward
-#             if self.is_regression:
-#                 # activation
-#                 self.pred = nn.Sigmoid(self.logits)
-#
-#                 loss = self.loss_regression(self.logits, target)
-#                 n_correct = self.performance_regression(self.logits, target, threshold=self.threshold)
-#
-#             else:
-#                 loss = self.loss_cross_entropy(self.logits, target, smoothing)
-#                 n_correct = self.performance_multi(self.logits, target)
-#
-#             loss.backward()
-#
-#             # update parameters
-#             self.optimizer.step()
-#
-#             # note keeping
-#             total_loss += loss.item()
-#
-#             non_pad_mask = target.ne(0)
-#             n_sample = non_pad_mask.sum().item()
-#             n_sample_total += n_sample
-#             n_sample_correct += n_correct
-#
-#         loss_per_word = total_loss / n_sample_total
-#         accuracy = n_sample_correct / n_sample_total
-#         return loss_per_word, accuracy
-#
-#     def eval_epoch(self, validation_data, device):
-#         ''' Epoch operation in evaluation phase '''
-#
-#         self.model.eval()
-#
-#         total_loss = 0
-#         n_sample_total = 0
-#         n_sample_correct = 0
-#
-#         with torch.no_grad():
-#
-#             # prepare data
-#             index, position, target = map(lambda x: x.to(device), validation_data)
-#
-#             # forward
-#             logits = self.model(index, position)
-#
-#             if self.is_regression:
-#                 loss = self.loss_regression(logits, target)
-#                 n_correct = self.performance_regression(logits, target, threshold=self.threshold)
-#
-#             else:
-#                 loss = self.loss_cross_entropy(logits, target)
-#                 n_correct = self.performance_multi(logits, target)
-#
-#             # note keeping
-#             total_loss += loss.item()
-#
-#             # note keeping
-#             total_loss += loss.item()
-#
-#             non_pad_mask = target.ne(0)
-#             n_sample = non_pad_mask.sum().item()
-#             n_sample_total += n_sample
-#             n_sample_correct += n_correct
-#
-#         loss_per_word = total_loss / n_sample_total
-#         accuracy = n_sample_correct / n_sample_total
-#         return loss_per_word, accuracy
-#
-#     def train(self, training_data, validation_data, epoch, device, smoothing, save_mode):
-#         assert save_mode in ['all', 'best']
-#         valid_losses = []
-#         for epoch_i in range(epoch):
-#             print('[ Epoch', epoch_i, ']')
-#
-#             start = time.time()
-#             train_loss, train_accu = self.train_epoch(training_data, device, smoothing=smoothing)
-#             self._logger_train.info('  - (Training)   ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
-#                                     'elapse: {elapse:3.3f} min'.format(
-#                 ppl=math.exp(min(train_loss, 100)), accu=100 * train_accu,
-#                 elapse=(time.time() - start) / 60))
-#
-#             start = time.time()
-#             valid_loss, valid_accu = self.eval_epoch(validation_data, device)
-#             self._logger_eval.info('  - (Validation) ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, ' \
-#                                    'elapse: {elapse:3.3f} min'.format(
-#                 ppl=math.exp(min(valid_loss, 100)), accu=100 * valid_accu,
-#                 elapse=(time.time() - start) / 60))
-#
-#             valid_losses += [valid_loss]
-#
-#             checkpoint = {
-#                 'models': self.model.state_dict(),
-#                 'optimizer_state_dict': self.optimizer.state_dict(),
-#                 'epoch': epoch_i}
-#
-#             if save_mode == 'all':
-#                 self.save_model(checkpoint, self.save_path + '_loss_{loss:3.3f}.chkpt'.format(loss=valid_loss))
-#
-#             if save_mode == 'best':
-#                 if valid_loss >= max(valid_loss):
-#                     self.save_model(checkpoint, self.save_path + '_loss_{loss:3.3f}.chkpt'.format(loss=valid_loss))
-#                     print('    - [Info] The checkpoint file has been updated.')
+    def val_epoch(self, device, step):
+        ''' Epoch operation in evaluation phase '''
+
+        self.model.eval()
+
+        total_loss = 0
+        batch_counter = 0
+
+        with torch.no_grad():
+
+            for batch in tqdm(
+                    self.dataloader.val_dataloader(), mininterval=1,
+                    desc='  - (Evaluation)   ', leave=False):  # training_data should be a iterable
+
+                # prepare data
+                feature_1, feature_2, y = map(lambda x: x.to(device), batch[1])
+
+                logits = self.model(feature_1, feature_2)
+
+                pred = logits.sigmoid()
+
+                if y.shape[-1] == 1:
+                    loss = mse_loss(pred, y)
+
+                else:
+                    loss = cross_entropy_loss(pred, y, smoothing=False)
+
+                acc = accuracy(pred, y, threshold=self.threshold)
+                _, _, precision_avg, recall_avg = precision_recall(pred, y, self.d_output, threshold=self.threshold)
+
+                self.val_logger.info(
+                    '[EVALUATION] - loss: { %3.4f },    acc: { %1.4f },    pre: { %1.4f },    rec: { %1.4f }' % (
+                        loss, acc, precision_avg, recall_avg))
+
+                # note keeping
+                total_loss += loss.item()
+                batch_counter += 1
+
+            loss_avg = total_loss / batch_counter
+
+            state_dict = self.early_stopping(loss_avg)
+
+            if state_dict['save']:
+                checkpoint = {
+                    'models': self.model.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                    'global_step': step}
+                self.save_model(checkpoint, self.save_path)
+
+            return state_dict['stop']
+
+    def train(self, training_dataloader, validation_dataloader, epoch, device, smoothing, save_mode):
+        assert save_mode in ['all', 'best']
+        for epoch_i in range(epoch):
+            print('[ Epoch', epoch_i, ']')
+
+            self.controller.set_epoch(epoch_i)
+            state_dict = self.train_epoch(device, smoothing)
+
+            if state_dict['step_to_stop']:
+                break
+
+        print('[INFO]: Finish Training, ends with %d epoch(s), %d batches and in total %d training steps.' % (
+        state_dict['epoch'], state_dict['batch'], state_dict['step']))
