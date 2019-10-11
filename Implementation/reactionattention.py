0f00a9b221c62740f2fa3aa8a8bea9fb63018c9e
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-10-11 18:27:11 -0400

    update implementation

--- Implementation/reactionattention.py
+++ Implementation/reactionattention.py
@@ -1,13 +1,26 @@
 from Dataloader.reactionattention import ReactionDataloader
 from Modules.reactionattention import ReactionAttentionStack, SelfAttentionStack, AlternateStack, ParallelStack
-from Models.reactionattention import ReactionModelLightning
+from Models.reactionattention import ReactionModel_
 from pytorch_lightning import Trainer
+import torch.nn as nn
+from torch.optim import Adam
+dataloader = ReactionDataloader('/home/oem/Projects/ciena_hackathon/data/', 1000, 0.01)
 
-dataloader = ReactionDataloader('/Users/sunjincheng/Desktop/Hackathon/data/allpm_anomaly/', 1000, 0.01)
 
-model = ReactionModelLightning(dataloader, ReactionAttentionStack, d_reactant=64, d_bottleneck=128, d_classifier=512,
-                               d_output=2)
+model = ReactionModel_('models/ckpt', 'logs', dataloader, Adam, ReactionAttentionStack, d_reactant=64, d_bottleneck=128, d_classifier=512,
+                               d_output=2, n_layers=6, )
 
-trainer = Trainer()
+# model.train(epoch=20, device='cuda', smoothing=False, save_mode='best')
 
-trainer.fit(model)
+# model = ReactionAttentionStack(10, 10, 10, 10, 10, 10)
+# model = nn.DataParallel(model)
+# para = model.parameters()
+# for i in para:
+#     print(i.is_cuda)
+
+
+model.load_model('models/ckpt-loss-0.0011362792271045802')
+pred = model.predict(dataloader.test_dataloader(), 'cuda')
+
+# import numpy as np
+# p = np.argmax(pred, axis=0)
