c056507892d3f8cdd7fc2ce7470527feed1ca6ba
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-10-11 18:25:24 -0400

    fix loading model problem

--- framework/model.py
+++ framework/model.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 from utils.loggings import logger
 import torch
+from collections import OrderedDict
 class Model():
     def __init__(self, save_path, log_path):
         self.save_path = save_path
@@ -11,6 +12,7 @@ class Model():
     def check_cuda(self):
         if torch.cuda.is_available():
             print("INFO: CUDA device exists")
+            return torch.cuda.is_available()
 
     @abstractmethod
     def loss(self, **kwargs):
@@ -27,12 +29,15 @@ class Model():
 
     def resume_checkpoint(self, checkpoint_path):
         checkpoint = torch.load(checkpoint_path)
-        self.model.load_state_dict(checkpoint['model_state_dict'])
+        state_dict = checkpoint['model_state_dict']
+        self.model.load_state_dict(state_dict)
         self.model.train()
 
     def save_model(self, checkpoint, save_path):
         torch.save(checkpoint, save_path)
 
-    def load_model(self, model_path):
-        self.model.load_state_dict(torch.load(model_path))
+    def load_model(self, checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+        state_dict = checkpoint['model_state_dict']
+        self.model.load_state_dict(state_dict)
         self.model.eval()
