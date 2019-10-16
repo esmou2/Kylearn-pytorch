95502c96fac908023626bf2e6cbb1cf5bbd72c31
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-10-16 18:27:08 -0400

    update

--- Implementation/reactionattention.py
+++ Implementation/reactionattention.py
@@ -8,9 +8,9 @@ dataloader = ReactionDataloader('/home/oem/Projects/ciena_hackathon/data/', 1000
 
 
 model = ReactionModel_('models/ckpt', 'logs', dataloader, Adam, ReactionAttentionStack, d_reactant=64, d_bottleneck=128, d_classifier=512,
-                               d_output=2, n_layers=6, )
+                               d_output=1, n_layers=6, threshold=0.5)
 
-# model.train(epoch=20, device='cuda', smoothing=False, save_mode='best')
+model.train(epoch=20, device='cuda', smoothing=False, save_mode='best')
 
 # model = ReactionAttentionStack(10, 10, 10, 10, 10, 10)
 # model = nn.DataParallel(model)
@@ -18,9 +18,13 @@ model = ReactionModel_('models/ckpt', 'logs', dataloader, Adam, ReactionAttentio
 # for i in para:
 #     print(i.is_cuda)
 
-
-model.load_model('models/ckpt-loss-0.0011362792271045802')
-pred = model.predict(dataloader.test_dataloader(), 'cuda')
+#
+# model.load_model('models/ckpt-loss-0.0860723660073497')
+pred, real = model.predict(dataloader.test_dataloader(), 'cuda')
 
 # import numpy as np
 # p = np.argmax(pred, axis=0)
+
+from utils.plot_curves import precision_recall, plot_pr_curve
+area, precisions, recalls, thresholds = precision_recall(pred, real)
+plot_pr_curve(recalls,precisions, auc=area)
