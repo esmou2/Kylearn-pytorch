03eb9560b542fb1323c9369a094cceb41e619bf7
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-10-16 18:23:52 -0400

    add function set_logger

--- framework/model.py
+++ framework/model.py
@@ -5,9 +5,12 @@ from collections import OrderedDict
 class Model():
     def __init__(self, save_path, log_path):
         self.save_path = save_path
+        self.log_path = log_path
         self.model = None
-        self.train_logger = logger(log_path+'train')
-        self.val_logger = logger(log_path+'test')
+
+
+    def set_logger(self):
+        self.train_logger = logger(self.log_path + 'train')
 
     def check_cuda(self):
         if torch.cuda.is_available():
