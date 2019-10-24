ebe5d047b13f875680a8d42326cc52ad1167cda8
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-10-23 20:13:06 -0400

    change to shuffle attention implementation

--- Implementation/shuffleselfattention.py
+++ Implementation/shuffleselfattention.py
@@ -1,19 +1,18 @@
 from Dataloader.reactionattention import ReactionDataloader
-from Models.reactionattention import ReactionModel_
+from Models.shuffleselfattention import ShuffleSelfAttentionModel
 from pytorch_lightning import Trainer
 import torch.nn as nn
 from torch.optim import Adam
 
-dataloader = ReactionDataloader('/home/oem/Projects/ciena_hackathon/data/', batch_size=500, val_size=0.01)
+dataloader = ReactionDataloader('/home/oem/Projects/ciena_hackathon/data/', batch_size=256, val_size=0.01)
 # dataloader = ReactionDataloader('/Users/sunjincheng/Desktop/Hackathon/data/allpm_anomaly/', batch_size=1000, val_size=0.01)
 
 
-model = ReactionModel_('models/ckpt', 'logs', dataloader, n_depth=64, d_bottleneck=128, d_classifier=512, n_layers=3,
-                       n_head=8, dropout=0.1, d_output=2, stack='ShuffleSelfAttention',
-                       expansion_layer='ChannelWiseConvExpansion'
-                       )
+model = ShuffleSelfAttentionModel('models/ckpt', 'logs', 16, 211, 256, 1, 0.5, mode='2d',
+                                  n_layers=4, n_head=8, n_channel=8, n_vchannel=8, dropout=0.1, use_bottleneck=True, d_bottleneck=128)
 
-model.train(epoch=20, device='cuda', smoothing=False, save_mode='best')
+
+model.train(20, dataloader.train_dataloader(), dataloader.val_dataloader(), device='cuda', save_mode='best', smoothing=False)
 
 # model.load_model('models/ckpt-loss-0.0860723660073497')
 # pred, real = model.predict(dataloader.test_dataloader(), 'cuda')
