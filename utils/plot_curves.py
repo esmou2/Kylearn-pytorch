31abc9c0d321fbe6a75eb6c937b12081afa66d96
Esma <esma.mouine@gmail.com>
2020-05-03 15:50:58 -0400

    fix

--- utils/plot_curves.py
+++ utils/plot_curves.py
@@ -4,17 +4,19 @@ from sklearn.metrics import roc_auc_score, roc_curve
 
 plt.rcParams['savefig.dpi'] = 300  # pixel
 plt.rcParams['figure.dpi'] = 300  # resolution
-plt.rcParams["figure.figsize"] = [5, 4] # figure size
+plt.rcParams["figure.figsize"] = [5, 4]  # figure size
+
 
 def precision_recall(y_pred, y_test):
     precisions, recalls, thresholds = precision_recall_curve(y_true=y_test, probas_pred=y_pred)
     area = auc(recalls, precisions)
     return area, precisions, recalls, thresholds
 
-def plot_pr_curve(recalls, precisions, auc, x_axis = 1):
-    plt.rcParams['savefig.dpi'] = 300  # pixel
-    plt.rcParams['figure.dpi'] = 300  # resolution
-    plt.rcParams["figure.figsize"] = [5, 4]  # figure size
+
+def plot_pr_curve(recalls, precisions, auc, x_axis=1, figure_size=[5, 4], pixel=300, resolution=300):
+    plt.rcParams['savefig.dpi'] = pixel  # pixel
+    plt.rcParams['figure.dpi'] = resolution  # resolution
+    plt.rcParams["figure.figsize"] = figure_size  # figure size
 
     plt.plot(recalls, precisions, color="darkorange", label='Precision-Recall curve (area = %0.3f)' % auc)
     plt.plot([1, 0], [0, 1], color='navy', lw=2, linestyle='--')
@@ -26,13 +28,14 @@ def plot_pr_curve(recalls, precisions, auc, x_axis = 1):
     plt.legend(loc="lower right")
     return plt
 
+
 def auc_roc(y_pred, y_test):
     auc = roc_auc_score(y_true=y_test, y_score=y_pred)
     fprs, tprs, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
     return auc, fprs, tprs, thresholds
 
-def plot_roc_curve(fprs, tprs, auc, x_axis = 1):
 
+def plot_roc_curve(fprs, tprs, auc, x_axis=1):
     plt.plot(fprs, tprs, color="darkorange", label='ROC curve (area = %0.3f)' % auc)
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, x_axis])
