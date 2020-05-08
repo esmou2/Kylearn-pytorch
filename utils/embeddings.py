63ae77397534d5dd97faf640fb26d646cee98d36
Esma <esma.mouine@gmail.com>
2020-05-07 20:26:53 -0400

    update get_w2v_embeddings method

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -2,15 +2,15 @@ import gensim
 import torch
 
 
-def get_w2v_embeddings(model_path, padding=False):
-    '''
+def get_w2v_embeddings_from_path(model_path, padding=False):
+    """
         Arguments:
             model_path {String} -- gensim word2vec model file path
             padding {Boolean} -- Whether to expand a 0 vector for padding
         Returns:
             embedding {Tensor, shape [n_vocabulary, embedding length]} -- word embedding vectors
             vector_length {Int} -- embedding length
-    '''
+    """
     # Load word2vec model
     word2vec = gensim.models.Word2Vec.load(model_path)
     vector_length = word2vec.vector_size
@@ -27,3 +27,21 @@ def one_hot_embedding(index, num_classes):
     diag = torch.eye(num_classes).byte()
     index = index.view(-1).tolist()
     return diag[index]
+
+
+def get_w2v_embeddings_from_model(word2vec: gensim.models.word2vec, padding: object = False) -> object:
+    """
+        Arguments:
+            model {String} -- gensim word2vec model
+            padding {Boolean} -- Whether to expand a 0 vector for padding
+        Returns:
+            embedding {Tensor, shape [n_vocabulary, embedding length]} -- word embedding vectors
+            vector_length {Int} -- embedding length
+    """
+    vector_length = word2vec.vector_size
+    embedding = torch.FloatTensor(word2vec.wv.vectors)
+    # Leave the first embedding to 0 for padding
+    if padding:
+        empty_embedding = torch.zeros_like(embedding[0]).unsqueeze(0)
+        embedding = torch.cat([empty_embedding, embedding])
+    return embedding, vector_length
